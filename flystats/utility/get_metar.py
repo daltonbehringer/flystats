abcd9670b38689adc4bd936ae12c6096a0afb8ec
Dalton Behringer <dalton.behringer@gmail.com>
2021-12-31 21:47:54 -0700

    data style update

--- flystats/utility/get_metar.py
+++ flystats/utility/get_metar.py
@@ -20,7 +20,16 @@ class _reader(object):
         
         self.site = data.station
         self.date = pd.to_datetime(data.valid)
-        self.metar = data.metar
+        self.wdir = data.drct
+        self.wspd = data.sknt
+        self.vsby = data.vsby
+        self.wgst = data.gust
+        self.skc1 = data.skyc1
+        self.skc2 = data.skyc2
+        self.skc3 = data.skyc3
+        self.skl1 = data.skyl1
+        self.skl2 = data.skyl2
+        self.skl3 = data.skyl3
 
         self._prep_data()
 
@@ -32,11 +41,40 @@ class _reader(object):
             "ICAO", np.ma.array(self.site), " ", "Station Identifier"
         )
         self.fields["date"] = var_to_dict(
-            "date", np.ma.array(self.date), "M/D/Y", "Seeding Aircraft Date",
+            "date", np.ma.array(self.date), " ", "Observation Date (UTC)",
         )
-        self.fields["metar"] = var_to_dict(
-            "metar", np.ma.array(self.metar), " ", "Raw METAR (5-min obs)",
+        self.fields["wdir"] = var_to_dict(
+            "wdir", np.ma.array(self.wdir), "deg", "Wind Direction",
  		)
+        self.fields["wspd"] = var_to_dict(
+            "wspd", np.ma.array(self.wspd), "kts", "Wind Speed",
+        )
+        self.fields["vsby"] = var_to_dict(
+            "vsby", np.ma.array(self.vsby), "statute miles", "Visibility",
+        )
+        self.fields["wgst"] = var_to_dict(
+            "wgst", np.ma.array(self.wgst), "kts", "Wind Gust",
+        )
+        self.fields["skc1"] = var_to_dict(
+            "skc1", np.ma.array(self.skc1), " ", "Sky Cover 1",
+        )
+        self.fields["skc2"] = var_to_dict(
+            "skc2", np.ma.array(self.skc2), " ", "Sky Cover 2",
+        )
+        self.fields["skc3"] = var_to_dict(
+            "skc3", np.ma.array(self.skc3), " ", "Sky Cover 3",
+        )
+        self.fields["skl1"] = var_to_dict(
+            "skl1", np.ma.array(self.skl1), "100 feet", "Sky Cover Level 1",
+        )
+        self.fields["skl2"] = var_to_dict(
+            "skl2", np.ma.array(self.skl2), "100 feet", "Sky Cover Level 2",
+        )
+        self.fields["skl3"] = var_to_dict(
+            "skl3", np.ma.array(self.skl3), "100 feet", "Sky Cover Level 3",
+        )
+
+
 
 def var_to_dict(standard_name, data, units, long_name):
 
@@ -46,3 +84,4 @@ def var_to_dict(standard_name, data, units, long_name):
     d["long_name"] = long_name
     d["standard_name"] = standard_name
     return d
+
