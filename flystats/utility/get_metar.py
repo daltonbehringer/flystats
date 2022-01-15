dfb0c5f3ab8d21ee927d36b916730323cbddbc4a
Dalton Behringer <dalton.behringer@gmail.com>
2022-01-15 15:11:49 -0800

    flight category

--- flystats/utility/get_metar.py
+++ flystats/utility/get_metar.py
@@ -33,6 +33,8 @@ class _reader(object):
 
         self._prep_data()
 
+        self._flight_cat()
+
     def _prep_data(self):
     
         self.fields = {}
@@ -74,6 +76,46 @@ class _reader(object):
             "skl3", np.ma.array(self.skl3), "100 feet", "Sky Cover Level 3",
         )
 
+        self.wdir = pd.to_numeric(self.wdir, errors = 'coerce')
+        self.wspd = pd.to_numeric(self.wspd, errors = 'coerce')
+        self.vsby = pd.to_numeric(self.vsby, errors = 'coerce')
+        self.wgst = pd.to_numeric(self.wgst, errors = 'coerce')
+
+        # self.skc1 = pd.to_numeric(self.skc1, errors = 'coerce')
+        # self.skc2 = pd.to_numeric(self.skc2, errors = 'coerce')
+        # self.skc3 = pd.to_numeric(self.skc3, errors = 'coerce')
+
+        self.skl1 = pd.to_numeric(self.skl1, errors = 'coerce')
+        self.skl2 = pd.to_numeric(self.skl2, errors = 'coerce')
+        self.skl3 = pd.to_numeric(self.skl3, errors = 'coerce') 
+
+    def _flight_cat(self):
+
+        self.ceiling_hgt = np.zeros((len(self.skc1)))
+
+        for i in range(len(self.date)):
+            if self.skc1[i] == 'BKN' or self.skc1[i] == 'OVC' or self.skc1[i] == 'VV':
+                self.ceiling_hgt[i] = self.skl1[i]
+            elif self.skc2[i] == 'BKN' or self.skc2[i] == 'OVC' or self.skc1[i] == 'VV':
+                self.ceiling_hgt[i] = self.skl2[i]
+            elif self.skc3[i] == 'BKN' or self.skc3[i] == 'OVC' or self.skc1[i] == 'VV':
+                self.ceiling_hgt[i] = self.skl3[i]
+
+        self.ceiling_hgt[self.ceiling_hgt == 0.] = np.nan
+
+        self.flight_cat = ['VFR']*len(self.date)
+
+        for i in range(len(self.date)):
+            if self.vsby[i] < 1. or self.ceiling_hgt[i] < 500.:
+                self.flight_cat.append('LIFR')
+            elif self.vsby[i] < 3. or self.ceiling_hgt[i] < 1000.:
+                self.flight_cat.append('IFR')
+            elif self.vsby[i] <= 5. or self.ceiling_hgt[i] <= 3000.:
+                self.flight_cat.append('MVFR')
+            else:
+                self.flight_cat.append('VFR')
+
+        # return flight_cat
 
 
 def var_to_dict(standard_name, data, units, long_name):
